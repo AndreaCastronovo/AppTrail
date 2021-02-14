01fa86ef34989c471d011400495fe55b55ec6f68
AndreaCastronovo <andreacastro@live.it>
2021-02-14 17:09:48 +0100

    Create all activity for Guida part.

--- app/src/main/java/com/example/apptrail_v2/Next3.java
+++ app/src/main/java/com/example/apptrail_v2/Next3.java
@@ -2,7 +2,13 @@ package com.example.apptrail_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Next3 extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class Next3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next3);
+
+        TextView text_cavalletto = findViewById(R.id.string_linkto_cavalletto);
+        text_cavalletto.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text_intervallometro = findViewById(R.id.string_linkto_intervallometro);
+        text_intervallometro.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void home(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
