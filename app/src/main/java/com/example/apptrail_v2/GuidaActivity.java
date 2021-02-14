985484d8de55890dcd328338a1680a138b5589a7
AndreaCastronovo <andreacastro@live.it>
2021-02-14 22:22:40 +0100

    Finish Guida part and main.

--- app/src/main/java/com/example/apptrail_v2/GuidaActivity.java
+++ app/src/main/java/com/example/apptrail_v2/GuidaActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class GuidaActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class GuidaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guida);
+
+        TextView text = findViewById(R.id.textGuida);
+        text.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void nord(View view) {
